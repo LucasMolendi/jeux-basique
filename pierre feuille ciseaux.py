08f982dfa6fab9129af0354d6792be55efb8273a
Lucas MOLENDI <lucasmolendi0@gmail.com>
2025-04-01 15:45:06 +0200

    annotation du code

--- pierre feuille ciseaux.py
+++ pierre feuille ciseaux.py
@@ -11,11 +11,16 @@ def shifumi(namej1,namej2,nbr_de_manche):
     j2_compteur = 0
     j1_compteur = 0 
     while j1_compteur < nbr_de_manche  and  j2_compteur < nbr_de_manche:
+        #on affiche le score de chaque joueur
         print(j1_compteur, j2_compteur)
+        #on demande au joueur de choisir entre pierre, feuille et ciseaux
         joueur1 = input("choissez entre pierre, feuille et ciseaux " )
+        #on choisit de manière alétaoire pour l'ordi
         i= random.randint(0,2)
         joueur2 = possibility[i]
+        #on test le choix du joueur pour savoir s'il est valide
         if joueur1 in possibility:
+            #on procède à la comparaison entre le choix du joueur et celui de l'ordi
             if joueur1 == joueur2:
                résultat = "aucun gagnant"
             elif joueur1 == possibility[0] and i == 1:
@@ -36,15 +41,20 @@ def shifumi(namej1,namej2,nbr_de_manche):
             elif joueur1 == possibility[1] and i == 0:
                 j1_compteur +=1
                 résultat = "victoire!!🎉"
+            #on affiche le résultat de la manche
             print(résultat)
         else:
+        #on affiche un message, si le joueur ne respecte pas les règles
             return "eh oh, on a dit pierre, feuille et ciseaux. tu fais quoi là!?"
-    
+    # on test la différence entre les deux compteurs pour savoir qui a gagné
     if j1_compteur>j2_compteur:
+        #on affiche le nom du gagnant
         return namej1 + " a gagné🎉"
     elif j1_compteur == j2_compteur:
+        #on affiche un message si il y a égalité
         return "égalité" 
     else:
+        #on affiche le nom du gagnant
         return namej2 + " a gagné"
     
 print(shifumi(namej1="Vladimir", namej2="Ordi",nbr_de_manche=longue ))
