3d78bd8ba6acba07db32e52d06c64ad65f5bd8cc
LucasMolendi <157105745+LucasMolendi@users.noreply.github.com>
2025-09-09 14:42:34 +0200

    rangement + création base intrus

---
+++ intrus/liste mots v1(sans trie).py
@@ -0,0 +1,23 @@
+#création d'une base de données contenant la liste de nom
+liste_mots = [
+    "Étoile",
+    "Mystère",
+    "Papillon",
+    "Crépitement",
+    "Lumière",
+    "Sérénité",
+    "Mélodie",
+    "Océan",
+    "Vertige",
+    "Souvenir",
+    "Marbre",
+    "Éclair",
+    "Silence",
+    "Aventure",
+    "Racine",
+    "Éphémère",
+    "Rêverie",
+    "Courage",
+    "Brouillard",
+    "Élan"
+]
