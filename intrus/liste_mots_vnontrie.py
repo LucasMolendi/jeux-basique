3e15f78ecf45ad50de0ae54a446470a46bb1c580
LucasMolendi <157105745+LucasMolendi@users.noreply.github.com>
2025-09-09 14:58:10 +0200

    création du markdown et première étape

--- intrus/liste_mots_vnontrie.py
+++ intrus/liste_mots_vnontrie.py
@@ -21,3 +21,10 @@ liste_mots = [
     "Brouillard",
     "Élan"
 ]
+#import random pour prnedre un mot au hasard
+from random import randint as rnd
+#définition de la variable fin qui est la taille de la liste -1 pour la dernière occurence
+fin= len(liste_mots)-1
+position = rnd(0,fin)
+print(position)
+print(liste_mots[position])
---
+++ intrus/étape de réalisation.md
@@ -0,0 +1,4 @@
+# Création d'un programme donnant une liste de mots avec un intrus
+
+## Etape 1 : base de donnée
+    Création d'une base de test avec 20 occurence inclus sur le doc du code pour le moment.
