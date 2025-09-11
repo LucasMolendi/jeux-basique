b7013a48cdf3b00d0fafdfc9951d6abbd7134119
LucasMolendi <157105745+LucasMolendi@users.noreply.github.com>
2025-09-11 10:37:53 +0200

    passage en fonction + mise a jour du readme

--- intrus/liste_mots_vnontrie.py
+++ intrus/liste_mots_vnontrie.py
@@ -23,8 +23,16 @@ liste_mots = [
 ]
 #import random pour prnedre un mot au hasard
 from random import randint as rnd
-#définition de la variable fin qui est la taille de la liste -1 pour la dernière occurence
-fin= len(liste_mots)-1
-position = rnd(0,fin)
-print(position)
-print(liste_mots[position])
+
+def lecture():
+    #définition de la variable fin qui est la taille de la liste -1 pour la dernière occurence et dans la rendre insensible a la casse
+    fin= len(liste_mots)-1
+    #création de la liste contenat les mots a la fin
+    retour_mot = []
+    for i in range(4):
+        #création de la variable position qui prend une valeur aléatoire entre 0(première occcurence) et fin (don dernière occurence)
+        position = rnd(0,fin)
+        #ajout du mot pris au hasard dans la liste de rendu final
+        retour_mot.append(liste_mots[position])
+    return retour_mot
+print(lecture())
--- intrus/étape de réalisation.md
+++ intrus/étape de réalisation.md
@@ -5,3 +5,5 @@ le programme a pour mission finale de données une liste de mots et c a nous de 
 Création d'une base de test avec 20 occurences(**agrandi plus tard**).
 ## Etape 2 : algorithme de lecture
 Création d'un algorithme basique qui import randint et qui prend un mots aléatoire de la liste (**sur le doc de la base de données pour le moment**).
+## Etape 3 : mise en place de cette algorithme sour forme de fonction
+Transformation de toute les variable issolé en une fonction qui retourne maintenant une liste de 4 mots (***cependant il peux encore y avoir des doublons a corriger***)
